e49daa8a4d6292c47173ac2aba983aa83e3166c3
Faffyta <145945863+Faffyta@users.noreply.github.com>
2023-09-24 23:33:19 -0300

    script.js

--- script.js
+++ script.js
@@ -27,14 +27,6 @@ function btnEncriptar(){
     }
 }
 
-//Laves de encriptacion
-// `La letra "e" es convertida para "enter"`
-// `La letra "i" es convertida para "imes"`
-// `La letra "a" es convertida para "ai"`
-// `La letra "o" es convertida para "ober"`
-// `La letra "u" es convertida para "ufat"`
-
-
 function encriptar(stringEncriptada){
     let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
     stringEncriptada = stringEncriptada.toLowerCase()
